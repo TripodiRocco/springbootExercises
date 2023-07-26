b12d83d79d730f5764b2269b6be4047d51210f45
Rocco <rocco.tripodi1994@gmail.com>
2023-07-26 17:39:28 +0200

    Added entity Ingredient

---
+++ src/main/java/com/firstTimeSpring/demo/Exercise5/entities/Ingredient.java
@@ -0,0 +1,108 @@
+package com.firstTimeSpring.demo.Exercise5.entities;
+
+import jakarta.persistence.*;
+
+/*
+Exercise 1:
+
+1. Create an Entity called Ingredient
+2. Create a controller, service, repository
+3. Set up CRUD operations for this entity
+Drop your ingredient table in your db if it still exists to avoid issues/confusion!
+
+public class Ingredient {
+    private Long id;
+    private String name;
+    private boolean isVegetarian;
+    private boolean isVegan;
+    private boolean isGlutenFree;
+    private boolean isLactoseFree;
+}
+ */
+
+@Entity
+@Table
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private boolean isVegetarian;
+
+    @Column(nullable = false)
+    private boolean isVegan;
+
+    @Column(nullable = false)
+    private boolean isGlutenFree;
+
+    @Column(nullable = false)
+    private boolean isLactoseFree;
+
+
+    public Ingredient(){
+
+    }
+
+    public Ingredient(Long id, String name, boolean isVegetarian, boolean isVegan, boolean isGlutenFree, boolean isLactoseFree) {
+        this.id = id;
+        this.name = name;
+        this.isVegetarian = isVegetarian;
+        this.isVegan = isVegan;
+        this.isGlutenFree = isGlutenFree;
+        this.isLactoseFree = isLactoseFree;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isVegetarian() {
+        return isVegetarian;
+    }
+
+    public void setVegetarian(boolean vegetarian) {
+        isVegetarian = vegetarian;
+    }
+
+    public boolean isVegan() {
+        return isVegan;
+    }
+
+    public void setVegan(boolean vegan) {
+        isVegan = vegan;
+    }
+
+    public boolean isGlutenFree() {
+        return isGlutenFree;
+    }
+
+    public void setGlutenFree(boolean glutenFree) {
+        isGlutenFree = glutenFree;
+    }
+
+    public boolean isLactoseFree() {
+        return isLactoseFree;
+    }
+
+    public void setLactoseFree(boolean lactoseFree) {
+        isLactoseFree = lactoseFree;
+    }
+
+}
